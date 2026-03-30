1d66a0018e637a7672da636fa6764bd6a5a1c9a9
imogenlay <imogen@imogenlay.com>
2026-03-30 16:59:27 +1100

    Added color palette picker

---
+++ wwwroot/js/theme.js
@@ -0,0 +1,6 @@
+﻿window.setCssVariables = (variables) => {
+    const root = document.documentElement.style;
+    for (const [key, value] of Object.entries(variables)) {
+        root.setProperty(key, value);
+    }
+}
